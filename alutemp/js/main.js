f47518f40fb0751a04eb319106534ad4f1b4ad8a
Varfolomei <varfolomeisafronov@gmail.com>
2020-06-01 09:47:27 +0300

    Создана базовая структура сайта

---
+++ alutemp/js/main.js
@@ -0,0 +1,30 @@
+"use strict"; // Webp =======================================
+
+function testWebP(callback) {
+  var webP = new Image();
+
+  webP.onload = webP.onerror = function () {
+    callback(webP.height == 2);
+  };
+
+  webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+  if (support == true) {
+    document.querySelector("body").classList.add("webp");
+  } else {
+    document.querySelector("body").classList.add("no-webp");
+  }
+}); //=============================================
+// ibg =======================================
+
+function ibg() {
+  $.each($(".ibg"), function (index, val) {
+    if ($(this).find("img").length > 0) {
+      $(this).css("background-image", 'url("' + $(this).find("img").attr("src") + '")');
+    }
+  });
+}
+
+ibg(); //=============================================
